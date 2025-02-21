171f167124a451879ab69e7d4a32051cf45a900c
Simão Neto <simaomenezesneto@gmail.com>
2025-02-21 20:54:21 +0000

    [Estrutura Visual] feat: Criando Estrutura da Home

--- my-shop/src/pages/index.tsx
+++ my-shop/src/pages/index.tsx
@@ -1,6 +1,29 @@
 
+import camiseta1 from "../assets/camisetas/1.png"
+import camiseta2 from "../assets/camisetas/2.png"
+
+import { HomeContainer, Product } from "../styles/pages/home"
+import Image from "next/image"
+
 export default function Home() {
     return (
-        <h1>Hello Next.JS</h1>
+        <HomeContainer>
+            <Product>
+                <Image src={camiseta1} width={520} height={480} alt="" />
+                <footer>
+                    <strong>Camiseta X</strong>
+                    <span>R$ 79,85</span>
+                </footer>
+            </Product>
+
+            <Product>
+                <Image src={camiseta2} width={520} height={480} alt="" />
+                <footer>
+                    <strong>Camiseta X</strong>
+                    <span>R$ 79,85</span>
+                </footer>
+            </Product>
+
+        </HomeContainer>
     )
 }

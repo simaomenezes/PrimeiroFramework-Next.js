dcb8a0dd7e286dd3d0c8540ec1831689e5c6df6b
Simão Neto <simaomenezesneto@gmail.com>
2025-04-07 13:49:44 +0100

    feat: Dados de compra com sucesso.

--- my-shop/src/pages/api/checkout.ts
+++ my-shop/src/pages/api/checkout.ts
@@ -2,14 +2,14 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { stripe } from "../../lib/stripe";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const {priceId} = req.body;
+  const { priceId } = req.body;
 
-  if(req.method !== 'POST'){
-    return res.status(405).json({ error: "Method not allowed." })
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed." });
   }
 
-  if(!priceId){
-    return res.status(400).json({ error: ""})
+  if (!priceId) {
+    return res.status(400).json({ error: 'Price not found.' });
   }
 
   const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
